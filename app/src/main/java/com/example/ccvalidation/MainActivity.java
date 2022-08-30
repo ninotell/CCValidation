9e9cc0b591d7ab0d6338f424006634b68ca02b2e
ninotell <ninotell@gmail.com>
2022-08-29 21:16:08 -0300

    Mejoras en validacion y se agregan logos

--- app/src/main/java/com/example/ccvalidation/MainActivity.java
+++ app/src/main/java/com/example/ccvalidation/MainActivity.java
@@ -15,15 +15,28 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.LinkedHashMap;
+
 public class MainActivity extends AppCompatActivity {
     private boolean valid = false;
+    private static final LinkedHashMap<String, String> bins = new LinkedHashMap<String, String>() {{
+        put("589562", "NARANJA");
+        put("589657", "CABAL");
+        put("6042", "CABAL");
+        put("6043", "CABAL");
+        put("34", "AMERICAN EXPRESS");
+        put("35", "AMERICAN EXPRESS");
+        put("36", "AMERICAN EXPRESS");
+        put("37", "AMERICAN EXPRESS");
+        put("4", "VISA");
+        put("5", "MASTERCARD");
+    }};
     EditText CCNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button checkButton = findViewById(R.id.checkButton);
         CCNumber = (EditText) findViewById(R.id.CCNumber);
         CCNumber.addTextChangedListener(new TextWatcher() {
             @Override
@@ -37,27 +50,58 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void afterTextChanged(Editable s) {
-                checkCCNumber(s.toString());
-                if (s.toString().length() == 16) {
+                if (s == null && s.toString() == "") {
+                    return;
+                }
+                luhnAlgorithm(s.toString());
+                if (s.toString().length() >= 15) {
                     updateTextView();
                 }
+                String number = s.toString();
+                if (valid && number.length() >= 15){
+                    TextView card_number = (TextView) findViewById(R.id.number);
+                    switch (number.length()){
+                        case 15:
+                            if (getCardNameFromCardNumber(number) != "AMERICAN EXPRESS"){
+                                break;
+                            }
+                            card_number.setText(number.substring(0, 4) + " " + number.substring(4, 8) + " " + number.substring(8, 12) + " " + number.substring(12, 15));
+                            break;
+                        case 16:
+                            card_number.setText(number.substring(0, 4) + " " + number.substring(4, 8) + " " + number.substring(8, 12) + " " + number.substring(12, 16));
+                            break;
+                    }
+                }
+
+                ImageView provider = (ImageView) findViewById(R.id.provider);
+                switch (getCardNameFromCardNumber(s.toString())){
+                    case "VISA":
+                        provider.setImageResource(R.drawable.visa);
+                        break;
+                    case "MASTERCARD":
+                        provider.setImageResource(R.drawable.master);
+                        break;
+                    case "NARANJA":
+                        provider.setImageResource(R.drawable.naranja);
+                        break;
+                    case "AMERICAN EXPRESS":
+                        provider.setImageResource(R.drawable.amex);
+                        break;
+                    case "CABAL":
+                        provider.setImageResource(R.drawable.cabal);
+                        break;
+                    default:
+                        provider.setImageResource(0);
+                        break;
+                };
             }
         });
     }
 
 
-    public void checkCCNumber(String number) {
-        ImageView provider = (ImageView) findViewById(R.id.provider);
-        TextView card_number = (TextView) findViewById(R.id.number);
+    public void luhnAlgorithm(String number) {
         int sum = 0;
         int position = 0;
-        int first = Integer.parseInt(number.substring(0, 1));
-        if (first == 5) {
-            provider.setImageResource(R.drawable.master);
-        } else if (first == 4) {
-            provider.setImageResource(R.drawable.visa);
-        }
-
         for (int i = number.length() - 1; i >= 0; i--) {
             position += 1;
             int digit = Integer.parseInt(Character.toString(number.charAt(i)));
@@ -69,34 +113,34 @@ public class MainActivity extends AppCompatActivity {
             }
             sum += digit;
         }
-
         if (sum % 10 == 0) {
             valid = true;
-            if (number.length() == 16) {
-                card_number.setText(number.substring(0, 4) + " " + number.substring(4, 8) + " " + number.substring(8, 12) + " " + number.substring(12, 16));
-            }
         } else {
             valid = false;
         }
     }
 
-    ;
+    private String getCardNameFromCardNumber(String nroTarjeta) {
+        if (nroTarjeta == null) {
+            return "";
+        }
+        for (String bin : bins.keySet()) {
+            if (nroTarjeta.startsWith(bin)) {
+                return bins.get(bin);
+            }
+        }
+        return "";
+    }
 
     private void updateTextView() {
-        TextView valid = findViewById(R.id.textViewCheck);
-        if (this.valid) {
-            valid.setTextColor(Color.GREEN);
-            valid.setText("VALIDA");
+        TextView tarjetaValida = findViewById(R.id.textViewCheck);
+        if (valid) {
+            tarjetaValida.setTextColor(Color.GREEN);
+            tarjetaValida.setText("VALIDA");
         } else {
-            valid.setTextColor(Color.RED);
-            valid.setText("INVALIDA");
+            tarjetaValida.setTextColor(Color.RED);
+            tarjetaValida.setText("INVALIDA");
         }
 
     }
-
-//    public void check(View view) {
-//        this.checkCCNumber();
-//        Log.d("PRUEBA", String.valueOf(valid));
-//        updateTextView();
-//    }
 }
